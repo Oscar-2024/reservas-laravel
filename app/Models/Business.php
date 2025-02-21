7269b6ff7daaba78f0c6084151a97e07b1e0dd00
Oscar <DeveloperMAC2023@gmail.com>
2025-02-21 11:05:49 -0300

    Creación de Modelos

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Business extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'phone',
+        'address',
+        'image',
+        'max_future_days',
+        'slot_duration',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'name' => 'string',
+            'phone' => 'string',
+            'address' => 'string',
+            'image' => 'string',
+            'max_future_days' => 'int',
+            'slot_duration' => 'int',
+        ];
+    }
+
+    public function schedules(): HasMany
+    {
+        return $this->hasMany(Schedule::class);
+    }
+
+    public function slots(): HasMany
+    {
+        return $this->hasMany(Slot::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,29 +4,30 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'credit',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -43,6 +44,17 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'credit' => 'int',
         ];
+    }
+
+    public function bookings(): Hasmany
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function hasCredits(): bool
+    {
+        return $this->credit > 0;
     }
 }
