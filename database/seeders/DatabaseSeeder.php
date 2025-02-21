b0b5f75474ec2619ed62124c0213867859fafaf7
Oscar <DeveloperMAC2023@gmail.com>
2025-02-21 12:17:54 -0300

    Creación de Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Business;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,107 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Cliente 1',
+            'email' => 'cliente1@example.com',
+            'credit' => 5,
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Cliente 2',
+            'email' => 'cliente2@example.com',
+            'credit' => 10,
+        ]);
+
+        $business = Business::create([
+            'name' => 'Negocio 1',
+            'phone' => '1234567890',
+            'address' => 'Calle 123',
+            'image' => fake()->imageUrl(word: 'Negocio 1'),
+            'max_future_days' => 7,
+            'slot_duration' => 30,
+        ]);
+
+        $business->schedules()->createMany([
+            [
+                'day_of_week' => 1,
+                'open_time' => '08:00',
+                'close_time' => '17:00',
+            ],
+            [
+                'day_of_week' => 2,
+                'open_time' => '08:00',
+                'close_time' => '17:00',
+            ],
+            [
+                'day_of_week' => 3,
+                'open_time' => '08:00',
+                'close_time' => '17:00',
+            ],
+            [
+                'day_of_week' => 4,
+                'open_time' => '08:00',
+                'close_time' => '17:00',
+            ],
+            [
+                'day_of_week' => 5,
+                'open_time' => '08:00',
+                'close_time' => '17:00',
+            ],
+            [
+                'day_of_week' => 6,
+                'open_time' => '08:00',
+                'close_time' => '12:00',
+            ],
+            [
+                'day_of_week' => 7,
+                'is_closed' => true,
+            ],
+        ]);
+
+        $business = Business::create([
+            'name' => 'Negocio 2',
+            'phone' => '0987654321',
+            'address' => 'Calle 321',
+            'image' => fake()->imageUrl(word: 'Negocio 2'),
+            'max_future_days' => 10,
+            'slot_duration' => 60,
+        ]);
+
+        $business->schedules()->createMany([
+            [
+                'day_of_week' => 1,
+                'open_time' => '08:00',
+                'close_time' => '20:00',
+            ],
+            [
+                'day_of_week' => 2,
+                'open_time' => '08:00',
+                'close_time' => '18:00',
+            ],
+            [
+                'day_of_week' => 3,
+                'open_time' => '08:00',
+                'close_time' => '17:00',
+            ],
+            [
+                'day_of_week' => 4,
+                'open_time' => '08:00',
+                'close_time' => '14:00',
+            ],
+            [
+                'day_of_week' => 5,
+                'open_time' => '08:00',
+                'close_time' => '17:00',
+            ],
+            [
+                'day_of_week' => 6,
+                'is_closed' => true,
+            ],
+            [
+                'day_of_week' => 7,
+                'is_closed' => true,
+            ],
         ]);
     }
 }
