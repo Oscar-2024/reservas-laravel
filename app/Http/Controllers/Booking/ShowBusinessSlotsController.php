94904558a25c300b3ada20ca932336128af72df9
Oscar <DeveloperMAC2023@gmail.com>
2025-02-21 14:58:04 -0300

    Creación de Controladores

---
+++ app/Http/Controllers/Booking/ShowBusinessSlotsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Booking;
+
+use App\Http\Controllers\Controller;
+use App\Models\Business;
+use Illuminate\View\View;
+
+class ShowBusinessSlotsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Business $business, int $year, int $month, int $day): View
+    {
+        $date = now()->setDate($year, $month, $day);
+
+        $business->load(['slots' => function ($query) use ($date) {
+            $query->with('booking')->where('slot_date', $date->format('Y-m-d'));
+        }]);
+
+        return view('business.slots', compact('business', 'date'));
+    }
+}
